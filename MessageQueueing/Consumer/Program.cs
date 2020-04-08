d3a81c12d047dcc49a8a812a75787b8541270489
Andrei Ursta <Andrei_Ursta@Dell.com>
2020-04-08 14:08:17 +0300

    added consumer

---
+++ MessageQueueing/Consumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += ConsumerReceived;
+                channel.BasicConsume("firstDeclare",true,consumer);
+                Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+            }
+        }
+
+        private static void ConsumerReceived(object sender, BasicDeliverEventArgs e)
+        {
+            var message = Encoding.UTF8.GetString(e.Body);
+            Console.WriteLine(" [x]Received {0}", message);
+        }
+    }
+}
