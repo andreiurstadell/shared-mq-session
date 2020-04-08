231412ead3598aeb4f41e3c919149ce4df580a4f
Andrei Ursta <Andrei_Ursta@Dell.com>
2020-04-08 14:40:05 +0300

    added 2nd consumer with nack

--- MessageQueueing/EmailConsumer/Program.cs
+++ MessageQueueing/EmailConsumer/Program.cs
@@ -1,24 +1,30 @@
-﻿using System;
-using System.Text;
-using RabbitMQ.Client;
+﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 
-namespace Consumer
+namespace EmailConsumer
 {
     class Program
     {
         private static IModel _channel;
-        //private static connection = 
         static void Main(string[] args)
         {
             var factory = new ConnectionFactory() { HostName = "localhost" };
             using (var connection = factory.CreateConnection())
             {
                 _channel = connection.CreateModel();
+                _channel.QueueDeclare(queue: "emailQueue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                _channel.QueueBind("emailQueue", "firstExchange", string.Empty);
 
                 var consumer = new EventingBasicConsumer(_channel);
                 consumer.Received += ConsumerReceived;
-                _channel.BasicConsume("firstDeclare", false, consumer);
+                _channel.BasicConsume("emailQueue", false, consumer);
                 Console.WriteLine(" Press [enter] to exit.");
                 Console.ReadLine();
                 _channel.Close();
@@ -29,7 +35,7 @@ namespace Consumer
         private static void ConsumerReceived(object sender, BasicDeliverEventArgs e)
         {
             var message = Encoding.UTF8.GetString(e.Body);
-            Console.WriteLine(" [x]Doc Consumer has received {0}", message);
+            Console.WriteLine(" [x]Email Consumer has received {0}", message);
             _channel.BasicAck(e.DeliveryTag, false);
         }
     }
