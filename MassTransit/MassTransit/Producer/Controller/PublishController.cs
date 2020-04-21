6b532853ca7a84d637a69883d828ca9cccfa9ed5
Andrei Ursta <Andrei_Ursta@Dell.com>
2020-04-21 14:01:49 +0300

    added message publisher

---
+++ MassTransit/MassTransit/Producer/Controller/PublishController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Producer.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublishController : ControllerBase
+    {   
+        private IPublishEndpoint _endpoint;
+
+        public PublishController(IPublishEndpoint publishEndpoint)
+        {
+            _endpoint = publishEndpoint;
+        }
+        public async Task Publish()
+        {
+            await _endpoint.Publish<IMessage>(new {Content = "msg"});
+        }
+    }
+}
---
+++ MassTransit/MassTransit/Producer/IMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Producer
+{
+    interface IMessage
+    {
+        string Content { get;set;}
+    }
+}
